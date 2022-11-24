95438f66be72141fa1945c08ee89c4d30117b4b2
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-24 22:04:56 +0600

    all categories api added

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
+
 app.use(cors());
 app.use(express.json());
+
+
+// Mongodb details 
+const uri = `mongodb+srv://${process.env.DB}:${process.env.DB_PASS}@cluster0.gaubth5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+        console.log('DB connected')
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+run();
+
+const categoriesCollections = client.db('HandPhoneStore').collection('categories');
+
+// get all the categories
+app.get('/categories', async (req, res) => {
+    try {
+        const query = {};
+        const result = await categoriesCollections.find(query).toArray();
+        res.send(result);
+    } catch (error) {
+        console.log(error)
+    }
+
+
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hello form the server')
