3128e27adaab1f3944ddf051dc892a912b140567
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-26 00:57:09 +0600

    verify seller middleware added

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 require('dotenv').config();
 
 
 app.use(cors());
 app.use(express.json());
+
 
 
 // Mongodb details 
@@ -31,6 +31,23 @@ run();
 const categoriesCollections = client.db('HandPhoneStore').collection('categories');
 const usersCollection = client.db('HandPhoneStore').collection('users');
 const productsCollection = client.db('HandPhoneStore').collection('products');
+
+
+
+
+//verify seller middleware 
+async function verifySeller(req, res, next) {
+    const { email } = req.query;
+    const query = { email };
+    const user = await usersCollection.findOne(query);
+    const userRole = user.role;
+    if (userRole !== 'seller') {
+        return res.send({
+            message: 'You are not a seller.'
+        })
+    }
+    next();
+}
 
 // get all the categories
 app.get('/categories', async (req, res) => {
@@ -82,17 +99,9 @@ app.get('/getrole', async (req, res) => {
 })
 
 //my products
-app.get('/myproducts', async (req, res) => {
+app.get('/myproducts', verifySeller, async (req, res) => {
     try {
         const { email } = req.query;
-        const query = { email };
-        const user = await usersCollection.findOne(query);
-        const userRole = user.role;
-        if (userRole !== 'seller') {
-            return res.send({
-                message: 'You are not a seller.'
-            })
-        }
         const products = await productsCollection.find({ sellerEmail: email }).toArray();
         res.send({
             products
@@ -100,6 +109,16 @@ app.get('/myproducts', async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+})
+
+//delete my products
+app.delete('/deletemyproduct', verifySeller, async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 
